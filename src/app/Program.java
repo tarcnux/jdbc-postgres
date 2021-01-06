47b2143da50d25c4ad2a26df6e193a80591952cf
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-06 12:00:07 -0300

    Gerar construtor, getters e settes de Product

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,19 +6,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection conn = DB.getConnection();
-	
-		Statement st = conn.createStatement();
-			
-		ResultSet rs = st.executeQuery("select * from tb_product");
-			
-		while (rs.next()) {
-			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
-		}
+		Product p = new Product();
+		p.setName("Pizza");
+		
+		System.out.println("Nome do produto: " + p.getName());
+//		Connection conn = DB.getConnection();
+//	
+//		Statement st = conn.createStatement();
+//			
+//		ResultSet rs = st.executeQuery("select * from tb_product");
+//			
+//		while (rs.next()) {
+//			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+//		}
 	}
 }
