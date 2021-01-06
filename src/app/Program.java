2b6587efe2817462783c65a195b140764d35835e
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-06 12:20:59 -0300

    Instanciar objetos Product

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,7 @@
 package app;
 
+
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,18 +14,28 @@ public class Program {
 
 	public static void main(String[] args) throws SQLException {
 		
+		Connection conn = DB.getConnection();
+	
+		Statement st = conn.createStatement();
+			
+		ResultSet rs = st.executeQuery("select * from tb_product");
+			
+		while (rs.next()) {
+			
+			Product p = instantiateProduct(rs);
+			System.out.println(p);
+			
+		}
+	}
+	
+	private static Product instantiateProduct(ResultSet rs) throws SQLException {
 		Product p = new Product();
-		p.setName("Pizza");
+		p.setId(rs.getLong("id"));
+		p.setDescription(rs.getString("description"));
+		p.setName(rs.getString("name"));
+		p.setImageUri(rs.getString("image_uri"));
+		p.setPrice(rs.getDouble("price"));
 		
-		System.out.println("Nome do produto: " + p.getName());
-//		Connection conn = DB.getConnection();
-//	
-//		Statement st = conn.createStatement();
-//			
-//		ResultSet rs = st.executeQuery("select * from tb_product");
-//			
-//		while (rs.next()) {
-//			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
-//		}
+		return p;
 	}
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -60,5 +60,13 @@ public class Product {
 	public void setImageUri(String imageUri) {
 		this.imageUri = imageUri;
 	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", description=" + description
+				+ ", imageUri=" + imageUri + "]";
+	}
+	
+	
 	
 }
